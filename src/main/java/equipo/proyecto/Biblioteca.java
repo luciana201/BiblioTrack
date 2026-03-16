5ab52657e9cbd7789ca80dc7eec70e3c6b5e4328
dayDPG <daya309.p0nc3@gmail.com>
2026-03-16 10:26:31 +0100

    actu clases

--- src/main/java/equipo/proyecto/Biblioteca.java
+++ src/main/java/equipo/proyecto/Biblioteca.java
@@ -8,4 +8,10 @@ public class Biblioteca {
     private ArrayList<Publicacion> publicaciones;
     private HashMap<String, ArrayList<Publicacion>> categorias;
     private ArrayList<Usuario> usuarios;
+    public Biblioteca(String pnombre){
+        this.nombre = pnombre;
+        this.publicaciones = new ArrayList<>();
+        this.categorias = new HashMap<>();
+        this.usuarios = new ArrayList<>();
+    }
 }
--- src/main/java/equipo/proyecto/Reseña.java
+++ src/main/java/equipo/proyecto/Reseña.java
@@ -8,4 +8,38 @@ public class Reseña {
     private LocalDate fecha;
     private Usuario usuario;
     private Publicacion publicacion;
+    public Reseña(String pcomentario, int pcalificacion, Usuario pusuario, Publicacion ppublicacion){
+        this.comentario = pcomentario;
+        this.calificacion = pcalificacion;
+        this.usuario = pusuario;
+        this.publicacion = ppublicacion;
+        this.fecha = LocalDate.now();
+    }
+    public int getId(){
+        return this.id;
+    }
+    public String getComentario(){
+        return this.comentario;
+    }
+    public int getCalificacion(){
+        return this.calificacion;
+    }
+    public LocalDate getFecha(){
+        return this.fecha;
+    }
+    public Usuario getUsuario(){
+        return this.usuario;
+    }
+    public Publicacion getPublicacion(){
+        return this.publicacion;
+    }
+    public void setComentario(String pcomentario){
+        this.comentario = pcomentario;
+    }
+    public void setCalificacion(int pcalificacion){
+        this.calificacion = pcalificacion;
+    }
+    public String toString(){
+        return "Reseña de " + this.usuario.getNombre() + " para " + this.publicacion.getTitulo() + ": " + this.comentario + " (Calificación: " + this.calificacion + "/10)";
+    }
 }
